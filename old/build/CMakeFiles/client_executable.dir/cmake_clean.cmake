7ec983e1766df4427ea99959c3c805b8effab3cb
Rafael Cunha <rafael.cunha@sap.com>
2025-04-13 18:00:20 -0300

    reorganize folders in OLD and NEW

---
+++ old/build/CMakeFiles/client_executable.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client_executable.dir/client/main_client.cpp.o"
+  "CMakeFiles/client_executable.dir/client/main_client.cpp.o.d"
+  "CMakeFiles/client_executable.dir/socket/classes/UdpClient.cpp.o"
+  "CMakeFiles/client_executable.dir/socket/classes/UdpClient.cpp.o.d"
+  "client_executable"
+  "client_executable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/client_executable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ old/build/CMakeFiles/server_executable.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/i746705/Desktop/UFRGS/SISOP2/sisop2/server/main_server.cpp" "CMakeFiles/server_executable.dir/server/main_server.cpp.o" "gcc" "CMakeFiles/server_executable.dir/server/main_server.cpp.o.d"
+  "/Users/i746705/Desktop/UFRGS/SISOP2/sisop2/socket/classes/UdpServer.cpp" "CMakeFiles/server_executable.dir/socket/classes/UdpServer.cpp.o" "gcc" "CMakeFiles/server_executable.dir/socket/classes/UdpServer.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
