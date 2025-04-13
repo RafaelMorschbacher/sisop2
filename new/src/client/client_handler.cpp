f825377fc6ad45d901ab07b84125445e2d98844a
Rafael Cunha <rafael.cunha@sap.com>
2025-04-13 18:46:46 -0300

    refactor: encapsulate client and server in classes

--- new/src/client/client_handler.cpp
+++ new/src/client/client_handler.cpp
@@ -6,6 +6,120 @@
 #include "Global.h"
 #include "Utils.h"
 
+class ClientHandler
+{
+private:
+    int clientSocket;
+    sockaddr_in broadcastAddr, serverAddr;
+    char buffer[BUFFER_SIZE];
+    int requestId;
+
+public:
+    ClientHandler() : clientSocket(-1), requestId(0) {}
+
+    ~ClientHandler()
+    {
+        if (clientSocket >= 0)
+        {
+            close(clientSocket);
+        }
+    }
+
+    bool initializeSocket(int broadcastPort)
+    {
+        // Create a UDP socket
+        clientSocket = socket(AF_INET, SOCK_DGRAM, 0);
+        if (clientSocket < 0)
+        {
+            std::cerr << "Error creating socket." << std::endl;
+            return false;
+        }
+
+        // Enable broadcast
+        int broadcastEnable = 1;
+        if (setsockopt(clientSocket, SOL_SOCKET, SO_BROADCAST, &broadcastEnable, sizeof(broadcastEnable)) < 0)
+        {
+            std::cerr << "ERROR enabling broadcast" << std::endl;
+            close(clientSocket);
+            return false;
+        }
+
+        // Set broadcast address
+        memset(&broadcastAddr, 0, sizeof(broadcastAddr));
+        broadcastAddr.sin_family = AF_INET;
+        broadcastAddr.sin_port = htons(broadcastPort);
+        broadcastAddr.sin_addr.s_addr = htonl(INADDR_BROADCAST);
+
+        return true;
+    }
+
+    bool performHandshake()
+    {
+        // Send broadcast message
+        int broadcastReturn = sendto(clientSocket, BROADCAST_MESSAGE, strlen(BROADCAST_MESSAGE), 0, (struct sockaddr *)&broadcastAddr, sizeof(broadcastAddr));
+        if (broadcastReturn < 0)
+        {
+            std::cerr << "ERROR sending broadcast message" << std::endl;
+            return false;
+        }
+
+        socklen_t serverAddrLen = sizeof(serverAddr);
+        int recvBytes = recvfrom(clientSocket, buffer, BUFFER_SIZE - 1, 0, (struct sockaddr *)&serverAddr, &serverAddrLen);
+        if (recvBytes < 0)
+        {
+            std::cerr << "Error receiving response from server." << std::endl;
+            return false;
+        }
+
+        // Test "acknowledged" message received from server
+        buffer[recvBytes] = '\0';
+        if (std::strcmp(buffer, ACKNOWLEDGEDMESSAGE) == 0)
+        {
+            std::cout << "Handshake successfully performed with server." << std::endl;
+            std::cout << "Server address: " << Utils::addressToString(serverAddr) << std::endl;
+            return true;
+        }
+        else
+        {
+            std::cout << "Handshake with server failed." << std::endl;
+            return false;
+        }
+    }
+
+    void mainLoop()
+    {
+        std::string message;
+        while (true)
+        {
+            std::cout << "Enter a number to send to the server (or 'exit' to disconnect): ";
+            std::getline(std::cin, message);
+
+            // Check if the user wants to exit or the input stream is closed
+            if (std::cin.eof() or message == EXIT_MESSAGE)
+            {
+                // Send exit message to the server and break the loop
+                std::cout << "Disconnecting from server..." << std::endl;
+                sendto(clientSocket, EXIT_MESSAGE, 4, 0, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+                break;
+            }
+
+            std::string fullMessage = std::to_string(++requestId) + " " + message;
+
+            sendto(clientSocket, fullMessage.c_str(), fullMessage.size(), 0, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+
+            socklen_t serverAddrLen = sizeof(serverAddr);
+            int recvBytes = recvfrom(clientSocket, buffer, BUFFER_SIZE - 1, 0, (struct sockaddr *)&serverAddr, &serverAddrLen);
+            if (recvBytes < 0)
+            {
+                std::cerr << "Error receiving response from server." << std::endl;
+                continue;
+            }
+            buffer[recvBytes] = '\0';
+            std::cout << "Server response: " << buffer << std::endl;
+        }
+    }
+};
+
 int main(int argc, char *argv[])
 {
     if (argc != 2)
@@ -21,93 +135,19 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    int clientSocket;
-    sockaddr_in broadcastAddr, serverAddr;
-    char buffer[BUFFER_SIZE];
-    int requestId = 0;
+    ClientHandler clientHandler;
 
-    // Create a UDP socket
-    clientSocket = socket(AF_INET, SOCK_DGRAM, 0);
-    if (clientSocket < 0)
+    if (!clientHandler.initializeSocket(broadcastPort))
     {
-        std::cerr << "Error creating socket." << std::endl;
-        return -1;
-    }
-
-    // Enable broadcast
-    int broadcastEnable = 1;
-    if (setsockopt(clientSocket, SOL_SOCKET, SO_BROADCAST, &broadcastEnable, sizeof(broadcastEnable)) < 0)
-    {
-        std::cerr << "ERROR enabling broadcast" << std::endl;
-        close(clientSocket);
         return 1;
     }
 
-    // Set broadcast address
-    memset(&broadcastAddr, 0, sizeof(broadcastAddr));
-    broadcastAddr.sin_family = AF_INET;
-    broadcastAddr.sin_port = htons(broadcastPort);
-    broadcastAddr.sin_addr.s_addr = htonl(INADDR_BROADCAST);
-
-    // Send broadcast message
-    int broadcastReturn = sendto(clientSocket, BROADCAST_MESSAGE, strlen(BROADCAST_MESSAGE), 0, (struct sockaddr *)&broadcastAddr, sizeof(broadcastAddr));
-    if (broadcastReturn < 0)
+    if (!clientHandler.performHandshake())
     {
-        std::cerr << "ERROR sending broadcast message" << std::endl;
-        close(clientSocket);
         return 1;
     }
 
-    socklen_t serverAddrLen = sizeof(serverAddr);
-    int recvBytes = recvfrom(clientSocket, buffer, BUFFER_SIZE - 1, 0, (struct sockaddr *)&serverAddr, &serverAddrLen);
-    if (recvBytes < 0)
-    {
-        std::cerr << "Error receiving response from server." << std::endl;
-        close(clientSocket);
-        return -1;
-    }
+    clientHandler.mainLoop();
 
-    // Test "acknowledged" message received from server
-    buffer[recvBytes] = '\0';
-    if (std::strcmp(buffer, ACKNOWLEDGEDMESSAGE) == 0) {
-        std::cout << "Handshake succesfully performed with server." << std::endl;
-        std::cout << "Server address: " << Utils::addressToString(serverAddr) << std::endl; 
-    }
-    else {
-        std::cout << "Handshake with server failed." << std::endl;
-        return -1;
-    }
-
-    // Main loop to send messages to the server
-    std::string message;
-    while (true)
-    {
-        std::cout << "Enter a number to send to the server (or 'exit' to disconnect): ";
-        std::getline(std::cin, message);
-
-        // Check if the user wants to exit or the input stream is closed
-        if (std::cin.eof() or message == EXIT_MESSAGE)
-        {
-            // Send exit message to the server  and break the loop
-            std::cout << "Disconnecting from server..." << std::endl;
-            sendto(clientSocket, EXIT_MESSAGE, 4, 0, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
-            break;
-        }
-
-        std::string fullMessage = std::to_string(++requestId) + " " + message;
-
-        sendto(clientSocket, fullMessage.c_str(), fullMessage.size(), 0, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
-
-        recvBytes = recvfrom(clientSocket, buffer, BUFFER_SIZE - 1, 0, (struct sockaddr *)&serverAddr, &serverAddrLen);
-        if (recvBytes < 0)
-        {
-            std::cerr << "Error receiving response from server." << std::endl;
-            continue;
-        }
-        buffer[recvBytes] = '\0';
-        std::cout << "Server response: " << buffer << std::endl;
-    }
-
-    close(clientSocket);
     return 0;
 }
--- new/src/server/server_handler.cpp
+++ new/src/server/server_handler.cpp
@@ -19,13 +19,112 @@
 #include "ClientInfo.h"
 #include "Global.h"
 
+class ServerHandler
+{
+public:
+    ServerHandler(int port) : port(port), serverSocket(-1) {}
+
+    bool initialize()
+    {
+        // Create a UDP socket
+        serverSocket = socket(AF_INET, SOCK_DGRAM, 0);
+        if (serverSocket < 0)
+        {
+            std::cerr << "Error creating server socket." << std::endl;
+            return false;
+        }
+
+        // Set server address
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_addr.s_addr = INADDR_ANY;
+        serverAddr.sin_port = htons(port);
+
+        // Bind the created socket to an address and port
+        if (bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
+        {
+            std::cerr << "Error binding socket to address." << std::endl;
+            close(serverSocket);
+            return false;
+        }
+
+        std::cout << "UDP server started on port " << port << "." << std::endl;
+        std::cout << "Main thread ID: " << std::this_thread::get_id() << "." << std::endl;
+        std::cout << "Current sum: " << std::to_string(globalSum) << std::endl;
+        std::cout << "Total requests: " << std::to_string(serverInfo.num_reqs) << std::endl;
+
+        return true;
+    }
+
+    void startListening()
+    {
+        char buffer[BUFFER_SIZE];
+
+        // Main loop to receive messages from clients
+        while (true)
+        {
+            socklen_t clientAddrLen = sizeof(clientAddr);
+            int recvBytes = recvfrom(serverSocket, buffer, BUFFER_SIZE - 1, 0,
+                                     (struct sockaddr *)&clientAddr, &clientAddrLen);
+            if (recvBytes <= 0)
+            {
+                std::cerr << "Error receiving data." << std::endl;
+                continue;
+            }
+
+            buffer[recvBytes] = '\0';
+            std::string message(buffer);
+
+            // Check if the message is a discovery message
+            if (std::strcmp(message.c_str(), BROADCAST_MESSAGE) == 0)
+            {
+                DiscoveryService::handleDiscoveryMessage(serverSocket, clientAddr, clientAddrLen);
+                continue;
+            }
+
+            // Check if the message is an exit message
+            if (std::strcmp(message.c_str(), EXIT_MESSAGE) == 0)
+            {
+                InterfaceService::handleExitMessage(serverSocket, clientAddr, clientAddrLen);
+                continue;
+            }
+
+            // If the message is not a discovery or exit message, process it
+            std::string clientKey = Utils::addressToString(clientAddr);
+            {
+                std::lock_guard<std::mutex> lock(clientsMutex);
+                auto it = clients.find(clientKey);
+                if (it != clients.end())
+                {
+                    {
+                        std::lock_guard<std::mutex> queueLock(it->second.queueMutex);
+                        it->second.messageQueue.push(Message{message, clientAddr, clientAddrLen});
+                    }
+                    it->second.condition_variable.notify_one();
+                }
+                else
+                {
+                    std::cout << "Message received from unregistered client: " << clientKey << "." << std::endl;
+                }
+            }
+        }
+    }
+
+    ~ServerHandler()
+    {
+        if (serverSocket >= 0)
+        {
+            close(serverSocket);
+        }
+    }
+
+private:
+    int port;
+    int serverSocket;
+    sockaddr_in serverAddr, clientAddr;
+};
 
 int main(int argc, char *argv[])
 {
-    int serverSocket;
-    sockaddr_in serverAddr, clientAddr;
-    char buffer[BUFFER_SIZE];
-
     if (argc != 2)
     {
         std::cerr << "Usage: " << argv[0] << " <port>" << std::endl;
@@ -39,82 +138,12 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // Create a UDP socket
-    serverSocket = socket(AF_INET, SOCK_DGRAM, 0);
-    if (serverSocket < 0)
+    ServerHandler serverHandler(port);
+    if (!serverHandler.initialize())
     {
-        std::cerr << "Error creating server socket." << std::endl;
         return -1;
     }
 
-    // Set server address
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
-    serverAddr.sin_port = htons(port);
-
-
-    // Bind the created socket to an address and port
-    if (bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
-    {
-        std::cerr << "Error binding socket to address." << std::endl;
-        close(serverSocket);
-        return -1;
-    }
-
-    std::cout << "UDP server started on port " << port << "." << std::endl;
-    std::cout << "Main thread ID: " << std::this_thread::get_id() << "." << std::endl;
-    std::cout << "Current sum: " << std::to_string(globalSum) << std::endl;
-    std::cout << "Total requests: " << std::to_string(serverInfo.num_reqs) << std::endl;
-
-    // Main loop to receive messages from clients
-    while (true)
-    {
-        socklen_t clientAddrLen = sizeof(clientAddr);
-        int recvBytes = recvfrom(serverSocket, buffer, BUFFER_SIZE - 1, 0,
-                                 (struct sockaddr *)&clientAddr, &clientAddrLen);
-        if (recvBytes <= 0)
-        {
-            std::cerr << "Error receiving data." << std::endl;
-            continue;
-        }
-
-        buffer[recvBytes] = '\0';
-        std::string message(buffer);
-
-        // Check if the message is a discovery message
-        if (std::strcmp(message.c_str(), BROADCAST_MESSAGE) == 0)
-        {
-            DiscoveryService::handleDiscoveryMessage(serverSocket, clientAddr, clientAddrLen);
-            continue;
-        }
-
-        // Check if the message is an exit message
-        if (std::strcmp(message.c_str(), EXIT_MESSAGE) == 0)
-        {
-            InterfaceService::handleExitMessage(serverSocket, clientAddr, clientAddrLen);
-            continue;
-        }
-
-        // If the message is not a discovery or exit message, process it
-        std::string clientKey = Utils::addressToString(clientAddr);
-        {
-            std::lock_guard<std::mutex> lock(clientsMutex);
-            auto it = clients.find(clientKey);
-            if (it != clients.end())
-            {
-                {
-                    std::lock_guard<std::mutex> queueLock(it->second.queueMutex);
-                    it->second.messageQueue.push(Message{message, clientAddr, clientAddrLen});
-                }
-                it->second.condition_variable.notify_one();
-            }
-            else
-            {
-                std::cout << "Message received from unregistered client: " << clientKey << "." << std::endl;
-            }
-        }
-    }
-
-    close(serverSocket);
+    serverHandler.startListening();
     return 0;
 }
